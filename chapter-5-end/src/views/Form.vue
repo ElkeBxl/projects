3064dbabbaa61c5c452b634e6a7a0813761aac45
Elke Heymans <31392019+ElkeBxl@users.noreply.github.com>
2019-10-14 08:37:54 +0200

    Updated Vuetify's mask to vue-the-mask
    Support of mask has been dropped from Vuetify's form components, official docs say to use vue-the-mask. This is already adapted in the workshop documentation but was not yet reflected in the end result

--- chapter-5-end/src/views/Form.vue
+++ chapter-5-end/src/views/Form.vue
@@ -4,77 +4,88 @@
       <h2>Thank you for your interest, we will contact you soon</h2>
       <div class="details text-xs-left">
         <h3>Customer details:</h3>
-        <p><strong>Name:</strong> {{name}}</p>
-        <p><strong>Email:</strong> {{email}}</p>
-        <p><strong>Phone:</strong> {{phone}}</p>
+        <p>
+          <strong>Name:</strong>
+          {{name}}
+        </p>
+        <p>
+          <strong>Email:</strong>
+          {{email}}
+        </p>
+        <p>
+          <strong>Phone:</strong>
+          {{phone}}
+        </p>
       </div>
       <v-btn to="/">Go to homepage</v-btn>
     </div>
     <v-form v-else class="text-xs-center" v-model="valid">
-      <v-text-field v-model="name" required
-                    label="Name" :rules="nameRules"></v-text-field>
-      <v-text-field v-model="email"
-                    required :rules="emailRules"
-                    label="E-mail"></v-text-field>
-      <v-text-field v-model="phone"
-                    required mask="(###) ### - ####"
-                    :rules="phoneRules"
-                    label="Phone"></v-text-field>
-      <v-btn @click="submit" :disabled="!valid">
-        Submit
-      </v-btn>
+      <v-text-field v-model="name" required label="Name" :rules="nameRules"></v-text-field>
+      <v-text-field v-model="email" required :rules="emailRules" label="E-mail"></v-text-field>
+      <v-text-field
+        v-model="phone"
+        required
+        v-mask="'(###) ### - ####'"
+        :rules="phoneRules"
+        label="Phone"
+      ></v-text-field>
+      <v-btn @click="submit" :disabled="!valid">Submit</v-btn>
     </v-form>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
+import { mask } from "vue-the-mask";
 
 export default {
+  directives: {
+    mask
+  },
   data() {
     return {
       submitted: false,
-      name: '',
-      email: '',
-      phone: '',
+      name: "",
+      email: "",
+      phone: "",
       valid: true,
       nameRules: [
-        v => !!v || 'Name is required',
-        v => v.length > 2 || 'Name must be more than 2 characters',
+        v => !!v || "Name is required",
+        v => v.length > 2 || "Name must be more than 2 characters"
       ],
       emailRules: [
-        v => !!v || 'Email is required',
+        v => !!v || "Email is required",
         v =>
           /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
-          'E-mail must be valid',
+          "E-mail must be valid"
       ],
       phoneRules: [
-        v => !!v || 'Phone is required',
-        v => v.length >= 7 || 'Phone must be at least 7 digits',
-      ],
+        v => !!v || "Phone is required",
+        v => v.length >= 7 || "Phone must be at least 7 digits"
+      ]
     };
   },
   methods: {
-    ...mapActions(['clearFavorites']),
+    ...mapActions(["clearFavorites"]),
     submit() {
       this.clearFavorites();
       this.submitted = true;
-    },
-  },
+    }
+  }
 };
 </script>
 
 
 <style scoped>
-  .form-wrapper {
-    padding: 40px;
-  }
+.form-wrapper {
+  padding: 40px;
+}
 
-  .details {
-    padding-top: 30px;
-  }
+.details {
+  padding-top: 30px;
+}
 
-  h3 {
-    padding-bottom: 20px;
-  }
+h3 {
+  padding-bottom: 20px;
+}
 </style>
